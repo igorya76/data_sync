3fb5a37b661b9195a54b76b242281f345a137ab5
Ryan Igo <igorya76@gmail.com>
2017-11-29 09:06:55 -0500

    problem identified -- should be last fix

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,9 @@ var Download = require('../models/downloads')
 
 router.get('/', async function(req, res){
   console.log('got it');
-  /*var sync = await Download.returnMatchingRet()
+  var sync = await Download.returnMatchingRet()
 
-  res.render('home/home', {data: sync})*/
-  res.render('home/home')
+  res.render('home/home', {data: sync})
 })
 
 router.get('/download', async function(req, res){
