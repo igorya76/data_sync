f7b66a46fc5d3e77759226b97db5374783276b1d
Ryan Igo <igorya76@gmail.com>
2018-01-11 15:23:13 -0500

    add procore sync log download

--- server/download.js
+++ server/download.js
@@ -9,7 +9,7 @@ var apiKey = 'n3K9lqmL730FeNnei97Q';
 var Download = require('../models/downloads');
 
 module.exports.downloadAPIData = async function (type){
-let models = ['projects', 'pcco','drawingsets','drawingsheets','rfis','submittals','shop_drawings','inspections','manpower','project_roles','document_watch_list','documents_monitored','parent','syncLog','milestones_current','milestones_log','directory','safety_reports','safety_items','dates',"commitments","prime_contracts","logs"]
+let models = ['projects', 'pcco','drawingsets','drawingsheets','rfis','submittals','shop_drawings','inspections','manpower','project_roles','document_watch_list','documents_monitored','parent','syncLog','milestones_current','milestones_log','directory','safety_reports','safety_items','dates',"commitments","prime_contracts",'procoreSyncLog', "logs",]
 
   for (var i = 0; i < models.length; i++){
   var ti = await  downloadData(models[i],type);
